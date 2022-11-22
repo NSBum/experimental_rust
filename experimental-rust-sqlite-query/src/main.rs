018b8f433fc83e2a08ccd1c8f3f03a732a6555db
Alan Duncan <duncan.alan@me.com>
2022-11-22 10:12:46 -0500

    Commented out the insert code

--- experimental-rust-sqlite-query/src/main.rs
+++ experimental-rust-sqlite-query/src/main.rs
@@ -16,20 +16,20 @@ fn main() -> Result<()> {
     cat_colors.insert(String::from("Blue"), vec!["Tigger", "Sammy"]);
     cat_colors.insert(String::from("Black"), vec!["Oreo", "Biscuit"]);
 
-    for (color, catnames) in &cat_colors {
-        conn.execute(
-            "INSERT INTO cat_colors (name) values (?1)",
-            &[&color.to_string()],
-        )?;
-        let last_id: String = conn.last_insert_rowid().to_string();
-
-        for cat in catnames {
-            conn.execute(
-                "INSERT INTO cats (name, color_id) values (?1, ?2)",
-                &[&cat.to_string(), &last_id],
-            )?;
-        }
-    }
+//  for (color, catnames) in &cat_colors {
+//         conn.execute(
+//             "INSERT INTO cat_colors (name) values (?1)",
+//             &[&color.to_string()],
+//         )?;
+//         let last_id: String = conn.last_insert_rowid().to_string();
+// 
+//         for cat in catnames {
+//             conn.execute(
+//                 "INSERT INTO cats (name, color_id) values (?1, ?2)",
+//                 &[&cat.to_string(), &last_id],
+//             )?;
+//         }
+//     }
     let mut stmt = conn.prepare(
         "SELECT c.name, cc.name from cats c
          INNER JOIN cat_colors cc
