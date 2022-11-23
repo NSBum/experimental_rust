92593957173d4fd41d772a6afb19ee37e4c1056f
Alan Duncan <duncan.alan@me.com>
2022-11-23 04:11:08 -0500

    added comments

--- rustripregex/src/main.rs
+++ rustripregex/src/main.rs
@@ -1,9 +1,10 @@
 use regex::Regex;
 
-
-fn main() -> Result<()> {
+fn main()  {
+    // using named capture group feature $vowel
+    // and the \x{} code point nomenclature
     let re = Regex::new(r"(?P<vowel>[аеяиыуюэщ])\x{0301}").unwrap();
-    let before = String::from("благови́дный");
+    let before = String::from("Уверя́ю вас, что подо́бная оши́бка никогда не повтори́тся.");
     let after = re.replace_all(&before, "$vowel");
     println!("{}", after);
-}?
+}
