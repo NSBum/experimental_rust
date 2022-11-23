dc3a53e9f5442b442941d016dd9f6673a5aa6a29
Alan Duncan <duncan.alan@me.com>
2022-11-23 05:40:35 -0500

    get command line argument in function

--- rustripregex/src/main.rs
+++ rustripregex/src/main.rs
@@ -1,10 +1,38 @@
 use regex::Regex;
+use std::env;
+
+#[allow(dead_code)]
+fn get_sentence_alt() -> String {
+    // this is an alternate way of doing it, but it
+    // requires checking the number of args rather
+    // than doing it in a more idiomatic way
+    let args: Vec<String> = env::args().collect();
+    let mut sentence = "Уверя́ю вас, что подо́бная оши́бка никогда не повтори́тся.".to_string();
+    println!("There are {} arguments. First is {}", args.len(), args[0]);
+    if args.len() < 2 {
+        println!("Since you didn't specify a sentence, using default.");
+    } else {
+        sentence = args[1].to_string();
+    }
+    sentence
+}
+
+fn get_sentence() -> String {
+    let args: Vec<String> = env::args().collect();
+    // rather than test args length, or worse, risking
+    // panic, we get get an Option<&String> and match
+    let param: Option<&String> = args.get(1);
+    match param {
+        Some(param) => param.to_string(),
+        None => "Уверя́ю вас, что подо́бная оши́бка никогда не повтори́тся.".to_string(),
+    }
+}
 
 fn main()  {
     // using named capture group feature $vowel
     // and the \x{} code point nomenclature
-    let re = Regex::new(r"(?P<vowel>[аеяиыуюэщ])\x{0301}").unwrap();
-    let before = String::from("Уверя́ю вас, что подо́бная оши́бка никогда не повтори́тся.");
+    let re = Regex::new(r"(?P<vowel>[аеояиыуюэщ])\x{0301}").unwrap();
+    let before = get_sentence();
     let after = re.replace_all(&before, "$vowel");
     println!("{}", after);
 }
