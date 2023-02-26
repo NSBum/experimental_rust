c5df6f2733754e1c4b350ff402a173f63b7cc331
Alan Duncan <duncan.alan@me.com>
2023-02-26 05:42:33 -0500

    Improve query for annotations

--- rust_query_list_ibooks/src/annotations.rs
+++ rust_query_list_ibooks/src/annotations.rs
@@ -1,22 +1,25 @@
 
 
 #[derive(Debug)]
-struct Annotation {
-    text_selection: String,
-    note: String,
+pub struct Annotation {
+    pub text_selection: String,
+    pub note: String,
 }
 
 pub fn annotations_query() -> String {
     let query = "select 
-    a.ZAEANNOTATION.ZANNOTATIONSELECTEDTEXT,
-    a.ZAEANNOTATION.ZANNOTATIONNOTE
+    ae.ZAEANNOTATION.ZANNOTATIONSELECTEDTEXT,
+    CASE 
+        WHEN ae.ZAEANNOTATION.ZANNOTATIONNOTE IS NULL THEN ''
+        ELSE ae.ZAEANNOTATION.ZANNOTATIONNOTE
+    END
 from
-    a.ZAEANNOTATION
+    ae.ZAEANNOTATION
 where 
-    a.ZAEANNOTATION.ZANNOTATIONASSETID = ?1
-    AND a.ZAEANNOTATION.ZANNOTATIONSELECTEDTEXT NOT NULL
+    ae.ZAEANNOTATION.ZANNOTATIONASSETID = ?1
+    AND ae.ZAEANNOTATION.ZANNOTATIONSELECTEDTEXT NOT NULL
 order by ZPLLOCATIONRANGESTART ASC, ZANNOTATIONCREATIONDATE ASC";
-    String::from(query);
+    String::from(query)
 }
 
 #[cfg(test)]
@@ -25,10 +28,10 @@ mod test {
 
     #[test]
     fn test_can_return_query() {
-        let actual_chunks = annotations_query().split(" ").collect();
+        let query = annotations_query();
+        let actual_chunks:Vec<&str> = query.split(" ").collect();
         let actual = actual_chunks[0];
         let expected = String::from("select");
         assert_eq!(actual, expected);
-        
     }
 }
